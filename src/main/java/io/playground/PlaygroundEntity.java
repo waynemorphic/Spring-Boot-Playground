3791ef20659e2d702ed6c496fd5d268950affc76
waynemorphic <wayneklyde@gmail.com>
2023-11-26 14:46:35 +0300

    Rename ..Table to ..Entity

--- src/main/java/io/playground/PlaygroundEntity.java
+++ src/main/java/io/playground/PlaygroundEntity.java
@@ -1,58 +1,45 @@
 package io.playground;
 
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /*
 * Entity shows that the class is created as a record in the database using persistence package
 * Table indicates that the class and its member variables will be table and columns
 * */
 
+@Getter
+@Setter
 @Entity
-@Table
-public class PlaygroundTable {
+@Table(name = "plaground")
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlaygroundEntity {
     @Id
-    @Column
-    private int Id;
-    @Column
+    private Long id;
     private String name;
-    @Column
     private String description;
 
-    // constructors
-    public PlaygroundTable(){}
-
-    public PlaygroundTable(int Id, String name, String description){
-        super();
-        this.Id = Id;
-        this.name = name;
-        this.description = description;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof PlaygroundEntity))
+            return false;
+        PlaygroundEntity that = (PlaygroundEntity) o;
+        return Objects.equals(getId(), that.getId()) && Objects.equals(getName(),
+            that.getName()) && Objects.equals(getDescription(), that.getDescription());
     }
 
-    // Getters and setter methods
-    public int getId() {
-        return Id;
-    }
-
-    public void setId(int id) {
-        Id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getDescription());
     }
 }
